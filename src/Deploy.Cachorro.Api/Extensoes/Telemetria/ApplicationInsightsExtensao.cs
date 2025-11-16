fda7c92a23842b0d6dbfe0daf017142e184fc90f
Nathan Cirillo <nathan_cirillo@hotmail.com>
2025-11-16 09:57:25 -0300

    feat: organizando a telemetria usando ocp

---
+++ src/Deploy.Cachorro.Api/Extensoes/Telemetria/ApplicationInsightsExtensao.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
+
+namespace Deploy.Cachorro.Api.Extensoes.Telemetria
+{
+    public static class ApplicationInsightsExtensao
+    {
+        public static void AddTelemetria(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.AddApplicationInsightsTelemetry(options =>
+            {
+                options.ConnectionString = configuration.GetSection("ConnectionsString:ApplicationInsights").Value;
+            })
+           .ConfigureTelemetryModule<QuickPulseTelemetryModule>((module, o) =>
+           {
+               module.AuthenticationApiKey = configuration.GetSection("ApplicationInsights:Api-Key").Value;
+           });
+        }
+    }
+}
---
+++ src/Deploy.Cachorro.Domain/EntidadeBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.Cachorro.Domain
+{
+    public class EntidadeBase<T>
+    {
+        public T Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
